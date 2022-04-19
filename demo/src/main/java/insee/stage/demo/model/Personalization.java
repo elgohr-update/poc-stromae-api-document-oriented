2ca9b4901f5cb3ad7e4cc00894d591ac795fe126
Bouttes Hugo <ZV2AOJ@ad.insee.intra>
2022-04-19 13:10:46 +0200

    Mon premier commit

--- demo/src/main/java/insee/stage/demo/model/Personalization.java
+++ demo/src/main/java/insee/stage/demo/model/Personalization.java
@@ -9,5 +9,42 @@ public class Personalization {
     private String state;
     private int currentPage;
 
+    public Personalization(String idStateData, Date date, String state, int currentPage) {
+        this.idStateData = idStateData;
+        this.date = date;
+        this.state = state;
+        this.currentPage = currentPage;
+    }
 
+    public String getIdStateData() {
+        return idStateData;
+    }
+
+    public void setIdStateData(String idStateData) {
+        this.idStateData = idStateData;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(int currentPage) {
+        this.currentPage = currentPage;
+    }
 }
